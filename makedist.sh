0caaa5970a6d88c807c88126fb3ba3e2b27400db
Thorsten Koch <koch@zib.de>
2002-10-20 09:27:24 +0000

    Release 1.04 draussen.

--- makedist.sh
+++ makedist.sh
@@ -1,2 +1,2 @@
-tar -cvzf zimpl-1.03.tgz --exclude="*CVS*" --exclude="*cvs*" --exclude="*~" --exclude="*.o" --exclude="*.d" --exclude="*TODO" --exclude="lint.out" zimpl/LICENSE zimpl/README zimpl/src zimpl/doc/*.ps zimpl/example
+tar -cvzf zimpl-1.04.tgz --exclude="*CVS*" --exclude="*cvs*" --exclude="*~" --exclude="*.o" --exclude="*.d" --exclude="*TODO" --exclude="lint.out" zimpl/LICENSE zimpl/README zimpl/src zimpl/doc/*.ps zimpl/example
 
