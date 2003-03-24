754cc65a7ef5409adc15827ff5b46b9f3fb9af6a
Thorsten Koch <koch@zib.de>
2003-03-24 09:01:30 +0000

    Release 1.05 ist fertig.

--- makedist.sh
+++ makedist.sh
@@ -1,2 +1,12 @@
-tar -cvzf zimpl-1.05.tgz --exclude="*CVS*" --exclude="*cvs*" --exclude="*~" --exclude="*.o" --exclude="*.d" --exclude="*TODO" --exclude="lint.out" zimpl/LICENSE zimpl/README zimpl/COPYING zimpl/CHANGELOG zimpl/Makefile zimpl/make zimpl/src zimpl/doc/*.pdf zimpl/check
+tar -cvzf zimpl-1.05.tgz \
+--exclude="*CVS*" \
+--exclude="*cvs*" \
+--exclude="*~" \
+--exclude="*.o" \
+--exclude="*.d" \
+--exclude="*TODO" \
+--exclude="lint.out" \
+--exclude=".\#*" \
+zimpl/LICENSE zimpl/README zimpl/COPYING zimpl/CHANGELOG \
+zimpl/Makefile zimpl/make zimpl/src zimpl/doc/*.pdf zimpl/check
 
