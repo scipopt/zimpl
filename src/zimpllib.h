85bd563d9daaf21012508a7f536adfa76a5a9781
Thorsten Koch <koch@zib.de>
2006-01-20 20:13:08 +0000

    Hatte zimpllib.h vergessen.

---
+++ src/zimpllib.h
@@ -0,0 +1,37 @@
+#pragma ident "@(#) $Id: zimpllib.h,v 1.1 2006/01/20 20:13:08 bzfkocht Exp $"
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*   File....: zimpllib.h                                                    */
+/*   Name....: Zimpl library                                                 */
+/*   Author..: Thorsten Koch                                                 */
+/*   Copyright by Author, All rights reserved                                */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*
+ * Copyright (C) 2005 by Thorsten Koch <koch@zib.de>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#ifndef _ZIMPLLIB_H_
+#define _ZIMPLLIB_H_
+
+#ifndef _BOOL_H_
+#error "Need to include bool.h before zimpllib.h"
+#endif
+
+/*lint -sem(zpl_read, nulterm(1)) */
+extern Bool zpl_read(const char* filename);
+
+#endif /* _ZIMPLLIB_H_ */
