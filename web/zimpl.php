e921f218411322189b2a4dfe31d2a4ff391e4591
Thorsten Koch <koch@zib.de>
2006-05-01 19:45:14 +0000

    Zwei Typos.

--- web/zimpl.php
+++ web/zimpl.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<!-- $Id: zimpl.php,v 1.35 2006/01/30 15:49:36 bzfkocht Exp $ -->
+<!-- $Id: zimpl.php,v 1.36 2006/05/01 19:45:14 bzfkocht Exp $ -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <meta name="author" content="Thorsten Koch" />
 <meta name="description" content="Zimpl Homepage" />
 <meta name="keywords" content="ZIMPL, Mathematical Modelling Language, Mathematical Programming, Optimization, Algebraic Modelling Language" />
-<meta name="date" content="$Date: 2006/01/30 15:49:36 $" />
+<meta name="date" content="$Date: 2006/05/01 19:45:14 $" />
 <meta name="robots" content="index|follow" />
 <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
 <link rel="stylesheet" href="zimpl.css" type="text/css" />
@@ -48,8 +48,8 @@ is available. Zimpl has even been successfully compiled for Windows using
 <li>Bug fixes</li>
 <li>Unions of indexed sets</li>
 <li>Reading of single values</li>
-<li>Declarition of SOSs</li>
-<li>Now usable a a library.</li>
+<li>Declaration of SOSs</li>
+<li>Now usable as a library.</li>
 </ul>
 <h3>Download</h3>
 <p>
@@ -157,7 +157,7 @@ Optimization at Work</a></dt>
  class="logobar" width="100" height="36" /></a>
 </div>
 <hr /><address>
-Last Update $Date: 2006/01/30 15:49:36 $ by
+Last Update $Date: 2006/05/01 19:45:14 $ by
 <a href="/mitarbeiter/mitarbeiter.pl?email=koch&amp;language=en">Thorsten Koch</a>
 <br />&copy; 2005 by Thorsten Koch,
 <a href="/impressum/index.en.html">Imprint</a>
