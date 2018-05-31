e626dab52dfdf2a96eb3ddfaf902e9172e54f538
Franziska Schloesser <schloesser@zib.de>
2018-05-31 12:14:54 +0200

    update some versions to 3.3.6 and improve makedist

--- makedist.sh
+++ makedist.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
-VERSION="3.3.5"
+#!/bin/bash -e
+
+VERSION="3.3.6"
 NAME="zimpl-$VERSION"
-rm -f $NAME
-ln -s . $NAME
-rm -f $NAME.tgz
-tar -cvzhf $NAME.tgz \
+
+echo ">>> Packaging $NAME."
+
+# remove old tarball
+rm -f $NAME.tar
+
+# compile a fresh documentation
+echo ">>> Compiling documentation."
+make doc > /dev/null 2>&1
+
+# create tarball
+echo ">>> Creating tarball."
+tar --transform "s|^|${NAME}/|" -chf $NAME.tar \
 --exclude="*CVS*" \
 --exclude="*cvs*" \
 --exclude="*~" \
@@ -13,21 +23,39 @@ tar -cvzhf $NAME.tgz \
 --exclude="*TODO" \
 --exclude="lint.out" \
 --exclude=".\#*" \
---exclude="$NAME/make/local/*" \
+--exclude="make/local/*" \
 --exclude="*.output" \
 --exclude="*OLD*" \
-$NAME/LICENSE $NAME/README $NAME/CHANGELOG \
-$NAME/Makefile $NAME/Makefile.nmake $NAME/make $NAME/src $NAME/doc/zimpl.pdf \
-$NAME/doc/zimpl.man $NAME/check \
-$NAME/example/chvatal_diet.zpl $NAME/example/facility_location.zpl \
-$NAME/example/queens[12345].zpl $NAME/example/steinerbaum.zpl \
-$NAME/example/tsp.zpl $NAME/example/tsp.dat \
-$NAME/CMakeLists.txt $NAME/zimpl-config.cmake.in \
-$NAME/cmake/Modules/FindGMP.cmake $NAME/cmake/Modules/FindPCRE.cmake
-rm -f $NAME
-echo ""
-echo "check version numbers in Makefile, doc/zimpl.tex (2x), doc/docu.c, makedist.sh and ($VERSION):"
-grep "^VERSION" Makefile
-grep " Version" doc/zimpl.tex
-grep "@version" doc/docu.c
-grep "ZIMPL_VERSION" src/zimpl/mme.h
+LICENSE README CHANGELOG \
+Makefile Makefile.nmake make src doc/zimpl.pdf \
+doc/zimpl.man check \
+example/chvatal_diet.zpl example/facility_location.zpl \
+example/queens[12345].zpl example/steinerbaum.zpl \
+example/tsp.zpl example/tsp.dat \
+CMakeLists.txt zimpl-config.cmake.in \
+cmake/Modules/FindGMP.cmake cmake/Modules/FindPCRE.cmake
+
+# check version numbers
+echo ">>> Checking version numbers in Makefile, doc/zimpl.tex (2x), doc/docu.c, makedist.sh, CMakeLists.txt, src/zimpl/mme.h and makedist.sh. (Should be $VERSION)"
+
+# find all version numbers
+VERSIONS=""
+VERSIONS="$VERSIONS\n$(grep "^VERSION" Makefile)"
+VERSIONS="$VERSIONS\n$(grep " Version" doc/zimpl.tex |grep -v date|grep -v License)"
+VERSIONS="$VERSIONS\n$(grep "@version" doc/docu.c)"
+VERSIONS="$VERSIONS\n$(grep " VERSION" CMakeLists.txt)"
+VERSIONS="$VERSIONS\n$(grep "ZIMPL_VERSION" src/zimpl/mme.h)"
+
+# boil the formerly found versionnumbers down to a short format
+VERSIONNUMBERS=$(echo "$VERSIONS" | grep  -o "[0-9.]*" | sed 's/\.//g')
+
+# check if the boiled down numbers coincide with the one defined above
+VERSIONNUMBER=$(echo "$VERSION" | sed 's/\.//g')
+while read -r line; do
+  if [ "${VERSIONNUMBER}" != "${line}" ]; then
+    echo "${VERSIONNUMBER} and ${line} do not coincide! Aborting."
+    exit 1
+  fi
+done <<< "${VERSIONNUMBERS}";
+
+echo ">>> Successfully packaged ${NAME}."
