bc26ea2d3af99885ea2de9487a6b43b95376824c
Stefan Vigerske <svigerske@gams.com>
2024-09-13 11:56:17 +0700

    update version to 3.6.2

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-VERSION="3.6.1"
+VERSION="3.6.2"
 NAME="zimpl-$VERSION"
 
 echo ">>> Packaging $NAME."
--- src/zimpl/mme.h
+++ src/zimpl/mme.h
@@ -30,7 +30,7 @@
 extern "C" {
 #endif
 
-#define ZIMPL_VERSION  361
+#define ZIMPL_VERSION  362
 
 /* the following is not in code.h because code.h needs mme.h anyway,
  * but we also need these declaratons.
