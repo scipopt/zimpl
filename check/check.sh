22364bcc8db2385f0bad210a7e4212c88222dddf
Stefan Vigerske <svigerske@gams.com>
2025-01-28 10:00:38 +0100

    update copyright years

--- check/check.sh
+++ check/check.sh
@@ -9,7 +9,7 @@
 #*                                                                           */
 #* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #*
-#* Copyright (C) 2007-2024 by Thorsten Koch <koch@zib.de>
+#* Copyright (C) 2007-2025 by Thorsten Koch <koch@zib.de>
 #* 
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
--- check/minlp.zpl
+++ check/minlp.zpl
@@ -8,7 +8,7 @@
 #*                                                                           *
 #* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 #*
-#* Copyright (C) 2011-2024 by Thorsten Koch <koch@zib.de>
+#* Copyright (C) 2011-2025 by Thorsten Koch <koch@zib.de>
 #* 
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
